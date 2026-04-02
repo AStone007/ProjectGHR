580ffc9f2888085c81961e2ce417b6cf72d61288
AStone007 <adamlukestone1@gmail.com>
2026-04-02 10:30:25 +0100

    Setup VCS

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    char[] fileinput;
+    printf("Enter file name or desitination:/n");
+    scanf("%s", fileinput);
+    FILE *fp = fopen(fileinput, "r");
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    char line[256];
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(fp);
     return 0;
 }
