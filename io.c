d125c98ca1b0a4aca597c961e600ba50ea92acf6
AStone007 <adamlukestone1@gmail.com>
2026-04-19 13:27:39 +0100

    Assign memory for the samples in the csv file

--- io.c
+++ io.c
@@ -11,3 +11,17 @@ int access_test(int a, int b) {
     int c = a + b;
     return c;
 }
+
+/*.........................................................
+ *........assign memory for samples in csv file............
+ *.........................................................*/
+WaveformSample* assign_memory(int num_samples) {
+
+    WaveformSample *data = malloc(num_samples * sizeof(WaveformSample));
+                                //num_samples defined later
+    if (data == NULL) {printf("ERROR: Trouble allocating memory for WaveformSample\n");}
+    else {printf("PROGRESS: Memory space allocated (Waveform sample)\n");}
+                                //to handle errors elegantly
+
+    return data; //return as only one result needed
+}
