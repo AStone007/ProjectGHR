2f671c10bef016d048400f247edbe6ffa98e047f
AStone007 <adamlukestone1@gmail.com>
2026-04-19 15:41:59 +0100

    Assign memory for the directory handling/organisation so that it is not messy in the memory

--- io.c
+++ io.c
@@ -15,13 +15,40 @@ int access_test(int a, int b) {
 /*.........................................................
  *........assign memory for samples in csv file............
  *.........................................................*/
+                            //use pointer as values will change
 WaveformSample* assign_memory(int num_samples) {
 
     WaveformSample *data = malloc(num_samples * sizeof(WaveformSample));
-                                //num_samples defined later
+                            //num_samples defined later
     if (data == NULL) {printf("ERROR: Trouble allocating memory for WaveformSample\n");}
     else {printf("PROGRESS: Memory space allocated (Waveform sample)\n");}
-                                //to handle errors elegantly
+                            //to handle errors elegantly
 
     return data; //return as only one result needed
+}
+
+/*.........................................................
+ *...........assign memory for organisation................
+ *.........................................................*/
+                            //to help with directory handling
+Directory* allocate_directory(int num_files) {
+
+    Directory *dir = malloc(sizeof(Directory));
+                            //assign and get the address of memory for Directory and stores address in dir
+    dir->files = malloc(num_files * sizeof(File));
+    dir->file_count = 0; //initialize
+
+    //check for allocation failure
+    if (dir == NULL) {
+        printf("ERROR: Trouble allocating memory for Directory\n");
+        return NULL;}
+    else {printf("PROGRESS: Memory space allocated (Directory)\n");}
+
+    if (dir->files == NULL) {
+        printf("ERROR: Trouble allocating memory for File array\n");
+        free(dir);
+        return NULL;}
+    else {printf("PROGRESS: Memory space allocated (File array)\n")}
+
+    return dir;
 }
